fb1f95de681e38d74f91a4622e227f21dec5ac03
danielcrubens <daniel.rubens@jointecnologia.com.br>
2026-06-23 16:28:08 -0300

    feat: configuring prisma client

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Prisma, User as UserModel } from '../generated/prisma/client';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Post('signup')
+  async signupUser(
+    @Body() userData: Prisma.UserCreateInput,
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+}
