9f4f647127ddbb8ea9300cf2c833e73cff9a01a6
danielcrubens <daniel.rubens@jointecnologia.com.br>
2026-06-24 18:17:25 -0300

    feat: creating token jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Prisma } from 'src/generated/prisma/client';
 
@@ -7,6 +7,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signin')
+  @HttpCode(HttpStatus.OK)
   signin(@Body() body: Prisma.UserCreateInput) {
     return this.authService.signin(body);
   }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,29 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Prisma, User } from 'src/generated/prisma/client';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Prisma } from 'src/generated/prisma/client';
 import { UserService } from '../user/user.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async signin(
     params: Prisma.UserCreateInput,
-  ): Promise<Omit<User, 'password'>> {
+  ): Promise<{ access_token: string }> {
     const user = await this.userService.user({ email: params.email });
     if (!user) throw new NotFoundException('User not found');
     const passwordMatch = await bcrypt.compare(params.password, user.password);
-    if (!passwordMatch) throw new NotFoundException('User not found');
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { password, ...result } = user;
-    return result;
+    if (!passwordMatch) throw new UnauthorizedException('User not found');
+    const payload = { sub: user.id };
+
+    return { access_token: await this.jwtService.signAsync(payload) };
   }
 }
